9c4746f6ca655b89c88629ce63442344bbda526e
simply-demo <supriyakulkarni2014@gmail.com>
2021-03-05 10:52:45 +0530

    added for learning purpose

--- app/Http/Controllers/sendmail.php
+++ app/Http/Controllers/sendmail.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Mail\markdownmail;
 use Mail;
 use Illuminate\Http\Request;
-
+//also added in github repositaory
 class sendmail extends Controller
 {
 	public function index()
